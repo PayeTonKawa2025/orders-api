efa929381532cb2b50c66d9c0e68d585e1fbebb3
lucasdpt <github@zertus.fr>
2025-06-12 13:28:22 +0200

    feat(RabbitMQ): add base for RabbitMQ

---
+++ src/main/java/fr/payetonkawa/orders/event/EventListener.java
@@ -0,0 +1,15 @@
+package fr.payetonkawa.orders.event;
+
+import fr.payetonkawa.orders.config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventListener {
+
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
+    public void handleEvent(String message) {
+        System.out.println("Received event: " + message);
+    }
+
+}
